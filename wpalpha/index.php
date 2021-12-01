b1b53388fd21b529127b8c2872051dc1207b3a0b
FunnyDev8149 <94663363+FunnyDev8149@users.noreply.github.com>
2021-12-01 12:32:24 +0300

    Update index.php

--- wpalpha/index.php
+++ wpalpha/index.php
@@ -23,13 +23,6 @@ if ( alpha_doing_ajax() && isset( $_GET['only_posts'] ) ) {
 	do_action( 'alpha_before_content' );
 
 	?>
-	<div class="page-content">
-	<?php
-	do_action( 'alpha_print_before_page_layout' );
-	alpha_get_template_part( 'posts/archive' );
-	do_action( 'alpha_print_after_page_layout' );
-	?>
-	</div>
 	<?php
 
 	do_action( 'alpha_after_content' );
